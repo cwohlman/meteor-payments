f72779999eb72130027b99d224f2e5d16abe2ade
Joshua Ohlman <codewriterohs@gmail.com>
2015-11-03 12:39:14 -0600

    Bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'cwohlman:payments',
   summary: 'Bulletproof payment processing logic.',
-  version: "0.4.5",
+  version: "0.5.0-1",
   git: 'https://github.com/cwohlman/meteor-payments.git'
 });
 
